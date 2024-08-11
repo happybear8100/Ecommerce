51c8b1958160aa75a01d6181b9e3c0c7ed437d4b
happybear8100 <happybear8100@outlook.com>
2024-08-11 11:14:01 +0900

    update:Navbar->Header

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Switch,Route} from "react-router-dom";
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 import ProductList from "./components/ProductList";
 import Details from "./components/Details";
 import Cart from "./components/Cart";
@@ -12,7 +12,7 @@ import Modal from './components/Modal';
 function App() {
   return (
     <React.Fragment>
-      <Navbar />
+      <Header />
       <Switch>
         <Route exact path="/" component={ProductList} />
         <Route path="/details" component={Details} />
